14179795256c400568458fe1b84a9c677418bd19
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 10:22:19 +0100

    Added comments and changed format

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,17 +1,18 @@
 public class Die {
+
+    // Creating a variable that will show the face value of the die:
     private int faceValue;
 
-
-    public Die(){
-        faceValue=1;
+    // Creating a constructor that sets the face value to be 1:
+    public Die() {
+        faceValue = 1;
     }
 
-    public int rollDice(){
-        final int MAX= 6;
-        return faceValue= (int) (Math.random()*MAX+1);
+    // Creating a roll method for when the player throws their dice, and thus changing the face value.
+    public int rollDice() {
+        // Setting the maximum value of a die to be:
+        final int MAX = 6;
+        return faceValue = (int) (Math.random() * MAX + 1);
     }
-
-
-
-    }
+}
 
