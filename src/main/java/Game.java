b61b06982c87fbf589da361d6ca4ec4ea32d6633
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2022-01-05 15:18:37 +0100

    Main can now start game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,11 @@
 import Fields.GameBoard;
+import GUI_Controllor.GUI_Controller;
 
 public class Game {
 
     public void startGame() {
         GameBoard gameBoard = new GameBoard();
-
+        GUI_Controller gui_controller = new GUI_Controller();
     }
 
 }
