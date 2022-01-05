ff1d6b6d914f4c5a7b209eecf5fb4ec0bcec7687
sabirinomar <sabirinomar19@gmail.com>
2022-01-05 17:49:25 +0100

    created ownableField

--- src/main/java/Fields/GameBoard.java
+++ src/main/java/Fields/GameBoard.java
@@ -36,7 +36,30 @@ public class GameBoard {
 
     private void createOwnableField() {
         OwnableField ownableField = new OwnableField();
-        ownableField.setFields(fields,1,10, Color.yellow, "hej");
+        ownableField.setFields(fields,1,1200, Color.blue, "Rødovrevej");
+        ownableField.setFields(fields,3, 1200,Color.blue,"Hvidovrevej");
+        ownableField.setFields(fields,6, 2000,Color.red,"Roskildevej");
+        ownableField.setFields(fields,8, 2000,Color.red,"Valby Langgade");
+        ownableField.setFields(fields,9, 2400,Color.red,"Allegade");
+        ownableField.setFields(fields,11, 2800,Color.yellow,"Frederiksberg Alle");
+        ownableField.setFields(fields,13, 2800,Color.yellow,"Bulowsvej");
+        ownableField.setFields(fields,14,3200,Color.yellow,"Gl. Kongevej");
+        ownableField.setFields(fields,16,3600,Color.gray,"Bernstorffsvej");
+        ownableField.setFields(fields,18,3600,Color.gray,"Hellerupvej");
+        ownableField.setFields(fields,19,4000,Color.gray,"Strandvejen");
+        ownableField.setFields(fields,21,4400,Color.RED,"Trianglen");
+        ownableField.setFields(fields,23,4400,Color.RED,"Østerbrogade");
+        ownableField.setFields(fields,24,4800,Color.RED,"Grønningen");
+        ownableField.setFields(fields,26, 5200,Color.white,"Bredgade");
+        ownableField.setFields(fields,27, 5200,Color.white,"Kgs.Nytorv");
+        ownableField.setFields(fields,29, 5600,Color.white,"Østergade");
+        ownableField.setFields(fields,31, 6000,Color.yellow,"Amagertorv");
+        ownableField.setFields(fields,32, 6000,Color.yellow,"Vimmelskaffet");
+        ownableField.setFields(fields,34, 6400,Color.yellow,"Nygade");
+        ownableField.setFields(fields,37, 7000,Color.magenta,"Frederiksberg gade");
+        ownableField.setFields(fields,39, 8000,Color.magenta,"Rådhuspladsen");
+
+
     }
 
 }
