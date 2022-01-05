1497114cd8aac23de1f20fc6df890d278af6d678
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 10:43:25 +0100

    Added comments and changed format

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,9 +3,9 @@ import java.awt.*;
 
 public class Car {
 
-    // Creating a constructor for the car class where the a color can be set
+    // Creating a constructor for the car class where the color can be set
     Car(Color color){
-        GUI_Car car = new GUI_Car();
-        car.setPrimaryColor(color);
+        GUI_Car car = new GUI_Car();  // Creating an instance of the GUI_Car class
+        car.setPrimaryColor(color);   // Setting the color
     }
 }
