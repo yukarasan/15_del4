33ea44c56872dc5fbce159a35382814dfafd1c09
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 15:12:54 +0100

    Removed main class in Field Class

--- src/main/java/Fields/Field.java
+++ src/main/java/Fields/Field.java
@@ -4,8 +4,5 @@ import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
 
 public class Field {
-
-    public static void main(String[] args) {
-        Field field = new Field();
-    }
+    Field field = new Field();
 }
--- src/main/java/Fields/GameBoard.java
+++ src/main/java/Fields/GameBoard.java
@@ -2,7 +2,6 @@ package Fields;
 
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
-import gui_main.GUI;
 
 import java.awt.*;
 
