46383d31fdd7021da3b6d96b8881dc1ac45090d9
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 10:22:52 +0100

    Added comments and changed format

--- src/main/java/DiceCup.java
+++ src/main/java/DiceCup.java
@@ -1,10 +1,5 @@
 public class DiceCup {
+    // Creating two dice objects:
     Die die1=new Die();
     Die die2=new Die();
-
-    public static void main(String[] args) {
-        DiceCup diceCup=new DiceCup();
-        System.out.println(diceCup.die1.rollDice());
-    }
-
 }
