3a1bfcf1561f931fcfe7bdbf685eb9011c6af394
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 18:41:54 +0100

    Changed name

--- src/test/java/ChooseColorTest.java
+++ src/test/java/ChooseColorTest.java
@@ -1,7 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
 
-public class Test {
+public class ChooseColorTest {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
