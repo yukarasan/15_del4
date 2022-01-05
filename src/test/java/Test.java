a1e974f567642e085c6f6a24506f7f6baddc1ca3
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 18:41:20 +0100

    An attempt to try some code for implementation in the game class

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,46 @@
-public class Test {
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean a = false, b = false;
+        boolean aAgain = false, bAgain = false;
+
+        while(true) {
+
+            System.out.println("Which color?");
+            String color = scanner.nextLine();
+
+            switch (color) {
+
+                case "a":
+                    if (a) {
+                        System.out.println("a is already taken");
+                        aAgain = true;
+                        break;
+                    }
+                case "b":
+                    if (b) {
+                        System.out.println("b is already taken");
+                        bAgain = true;
+                        break;
+                    }
+            }
+
+            if (Objects.equals(color, "a")) {
+                a = true;
+            } else if (Objects.equals(color, "b")) {
+                b = true;
+            }
+
+            if (aAgain) {
+                break;
+            } else if (bAgain) {
+                break;
+            }
+        }
+    }
 }
