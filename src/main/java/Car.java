7d62f276d6eb3be7c295c6a1d31d59cde61d5f2e
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-05 10:41:38 +0100

    Added comments and changed format

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,15 +1,11 @@
 import gui_fields.GUI_Car;
-import gui_main.GUI;
-
 import java.awt.*;
 
 public class Car {
-    Car(Color color){
-        GUI_Car car=new GUI_Car();
-        car.setPrimaryColor(color);
-    }
 
-    public static void main(String[] args) {
-        Car car= new Car(Color.blue);
+    // Creating a constructor for the car class where the a color can be set
+    Car(Color color){
+        GUI_Car car = new GUI_Car();
+        car.setPrimaryColor(color);
     }
 }
