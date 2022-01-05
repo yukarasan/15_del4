54f3bde6e3fa6a0f0c94791378c1506a1f725b18
noor <noorullainj@gmail.com>
2022-01-05 10:16:42 +0100

    We have made a dice and dicecup class

--- src/main/java/DiceCup.java
+++ src/main/java/DiceCup.java
@@ -1,3 +1,10 @@
 public class DiceCup {
+    Die die1=new Die();
+    Die die2=new Die();
+
+    public static void main(String[] args) {
+        DiceCup diceCup=new DiceCup();
+        System.out.println(diceCup.die1.rollDice());
+    }
 
 }
--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,3 +1,17 @@
 public class Die {
+    private int faceValue;
 
-}
+
+    public Die(){
+        faceValue=1;
+    }
+
+    public int rollDice(){
+        final int MAX= 6;
+        return faceValue= (int) (Math.random()*MAX+1);
+    }
+
+
+
+    }
+
