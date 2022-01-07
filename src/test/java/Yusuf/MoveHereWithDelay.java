5ae27313fd4242dd02b393470ddca54050de220a
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2022-01-07 22:17:33 +0100

    Made a test

---
+++ src/test/java/Yusuf/MoveHereWithDelay.java
@@ -0,0 +1,37 @@
+package Yusuf;
+
+import GUI_Controllor.GUI_Controller;
+import Main.Player;
+import gui_fields.GUI_Player;
+
+import java.util.concurrent.TimeUnit;
+
+public class MoveHereWithDelay {
+
+    public void movePlayerWithDelayToSpecificField(Player player, int move) {
+
+        int k = player.getSquare();
+
+        for (int i = k; i != move; i++) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            player.moveSquare(1,0);
+
+            // Moves the player to the square dependent on the value of move
+            System.out.println("Field: " + player.getSquare());
+        }
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        MoveHereWithDelay moveHereWithDelay = new MoveHereWithDelay();
+
+        player.moveSquare(25,0);
+        moveHereWithDelay.movePlayerWithDelayToSpecificField(player, 11);
+    }
+
+}
