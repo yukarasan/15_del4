e2196c362b07a74d5c21eff8b98028caad77254c
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-07 16:33:47 +0100

    Added MoveThreeFieldsBack card

---
+++ src/main/java/ChanceCards/MoveThreeFieldsBack.java
@@ -0,0 +1,13 @@
+package ChanceCards;
+
+import Main.Player;
+import gui_fields.GUI_Player;
+
+public class MoveThreeFieldsBack extends ChanceCardParent {
+    public void moveThreeFieldsBack(GUI_Player gui_player, Player player) {
+        gui.getInstance().displayChanceCard("Ryk tre felter tilbage");
+        gui.getInstance().showMessage(player.getName() + ", træk et chancekort fra bunken");
+
+        player.moveSquare(-3,0);
+    }
+}
