e3a7b529e742b2e980a215f411fc9673c5351092
Sohaib <s215784@dtu.dk>
2022-01-07 16:02:15 +0100

    small fix

--- src/main/java/ChanceCards/MoveToGroenningen.java
+++ src/main/java/ChanceCards/MoveToGroenningen.java
@@ -3,7 +3,7 @@ package ChanceCards;
 import Main.Player;
 import gui_fields.GUI_Player;
 
-public class MoveToGrønningen  extends ChanceCardParent  {
+public class MoveToGroenningen extends ChanceCardParent  {
 
     public void MoveToGrønningen (GUI_Player gui_player, Player player) {
 
