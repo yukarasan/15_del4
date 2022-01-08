847bf2ff816e1901d3ce4522f4b69d473b580e71
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-08 18:06:38 +0100

    Comments and layout

--- src/main/java/ChanceCards/ChanceCardParent.java
+++ src/main/java/ChanceCards/ChanceCardParent.java
@@ -3,6 +3,13 @@ package ChanceCards;
 import Fields.MoveWithADelay;
 import GUI_Controllor.GUI_Controller;
 
+/*
+In this class we've made one instance of a gui object from the GUI_Controller class and an instance of a moveWithADelay
+object from the MoveWithADelay class. These two objects will be used in all off the chance cards classes.
+We achieve this by making the chance cards inherent from this class by using the keyword "extends", and thereby making
+this class their parent class. This was done, so we did not have to instantiate these objects in every class.
+ */
+
 public class ChanceCardParent {
     GUI_Controller gui = new GUI_Controller();
     MoveWithADelay moveWithADelay = new MoveWithADelay();
