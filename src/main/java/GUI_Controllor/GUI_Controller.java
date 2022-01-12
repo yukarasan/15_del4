e7c7f4c6c44f4a34ddf5ff03e3a8d18a4dea9340
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-12 20:43:36 +0100

    Added static to getInstance method

--- src/main/java/GUI_Controllor/GUI_Controller.java
+++ src/main/java/GUI_Controllor/GUI_Controller.java
@@ -22,7 +22,7 @@ public final class GUI_Controller {
     then method will simply return the already created object gui_instance
      */
 
-    public GUI getInstance() {
+    public static GUI getInstance() {
         if (GUI_instance == null) {
             GUI_Controller gui = new GUI_Controller();
             GUI_instance = gui.GUI_controller();
