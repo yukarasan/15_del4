badfdd8630bc5273df3e81d6c77c4f5c3c5d9604
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-12 15:08:49 +0100

    Made ChanceCard abstract to demonstrate the theory behind for the report. Will revert later

--- src/main/java/ChanceCards/CarRepair.java
+++ src/main/java/ChanceCards/CarRepair.java
@@ -11,10 +11,16 @@ import gui_fields.GUI_Player;
 */
 
 public class CarRepair extends ChanceCardParent {
-    public void carRepair(GUI_Player gui_player, Player player) {
+    @Override
+    public void makePayment(GUI_Player gui_player, Player player) {
         gui.getInstance().displayChanceCard("Betal 3000 DKK for reparation af deres vogn");
         gui.getInstance().showMessage(player.getName() + ", træk et chancekort fra bunken");
         player.getAccount().setMoney(-3000);
         gui_player.setBalance(player.getAccount().getMoney());
+    }
+
+    @Override
+    public void acceptPayment(GUI_Player gui_player, Player player) {
+
     }
 }
