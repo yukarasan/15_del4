0d72c8fbe6dd8d2b2534f8a5bb57039682be8650
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-14 12:52:29 +0100

    Deleted unnecessary imports and changed layout

--- src/main/java/Fields/Jail.java
+++ src/main/java/Fields/Jail.java
@@ -1,12 +1,8 @@
 package Fields;
 
-import Main.*;
-import Main.Player;
 import gui_fields.GUI_Field;
-import gui_fields.GUI_Player;
 
 public class Jail extends UnownableField {
-
     public void jailFields(GUI_Field[] fields) {
         fields[30].setSubText("Gå i fængsel");
         fields[30].setTitle(" ");
@@ -14,7 +10,6 @@ public class Jail extends UnownableField {
         fields[10].setTitle(" ");
         fields[10].setSubText("Besøg fængsel");
     }
-
 }
 
 
