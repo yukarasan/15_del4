1781e961c54680b715fcaacb6c2604ba44eb111f
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-14 16:21:08 +0100

    Made test case and commented

--- src/test/java/Yusuf/ChooseColorTest.java
+++ src/test/java/Yusuf/ChooseColorTest.java
@@ -1,3 +1,5 @@
+package Yusuf;
+
 import java.util.Objects;
 import java.util.Scanner;
 
---
+++ src/test/java/Yusuf/NameFormat.java
@@ -0,0 +1,51 @@
+package Yusuf;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * This test-class tests for when a player enters their name. We've decided that the players should not be able to
+ * have a name that includes characters apart from the latin alphabet expect for "-".
+ * Since this game is targeted at a danish audience, we haven't considered including other alphabets.
+ */
+
+public class NameFormat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a name: ");
+
+            String name = scanner.nextLine();
+
+            String[] numbersInName = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            name = name.replaceAll(Arrays.toString(numbersInName), "");
+
+            String[] otherCharacters = {"?", "´", "`", "+", "|", "~", "^", "¨", "'", "*", "_", ".", ":", ";", ","
+                    + "=", ")", "(", "/", "&", "%", "¤", "#", "\"", "!", "\\", "§", "½", "<", ">"
+                    + "@", "£", "$", "€", "{", "}", "[", "]"};
+            name = name.replaceAll(Arrays.toString(otherCharacters), "");
+
+            char firstChar = name.charAt(0);  // localizing the first character at index 0
+            char lastChar = name.charAt(name.length() - 1);  // localizing the last character at index "name length"
+            String firstLetter = (Character.toString(firstChar)).toUpperCase(Locale.ROOT);  // Converting to a string and making it upper case
+            String restOfName = name.substring(1);  // Making a variable that separates the name at index 1
+
+            // Determining if input contains " " and then trimming the string before computing the rest of the statements
+            if (Character.toString(firstChar).matches(" ") || Character.toString(lastChar).matches(" ")) {
+                name = name.trim();
+                firstChar = name.charAt(0);
+                firstLetter = (Character.toString(firstChar)).toUpperCase(Locale.ROOT);
+                restOfName = name.substring(1);
+            }
+
+            // Detecting if the rest of the name contains any upper case letters, and converting them to lowercase.
+            restOfName = restOfName.toLowerCase();
+
+            name = firstLetter + restOfName;
+
+            System.out.println(name);
+        }
+    }
+}
