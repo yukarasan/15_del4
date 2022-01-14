e30f39e269f8df035890571efc475381a2acb105
Yusuf Kara <yusufkarasan@hotmail.com>
2022-01-14 16:27:13 +0100

    Commented class

--- src/test/java/Yusuf/ChooseColorTest.java
+++ src/test/java/Yusuf/ChooseColorTest.java
@@ -3,21 +3,23 @@ package Yusuf;
 import java.util.Objects;
 import java.util.Scanner;
 
+/**
+ * This test-class test for when a person chooses a color for their car. We've made it so, that the player can't
+ * choose the same color for their car.
+ */
+
 public class ChooseColorTest {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         boolean a = false, b = false;
         boolean aAgain = false, bAgain = false;
 
         while(true) {
-
             System.out.println("Which color?");
             String color = scanner.nextLine();
 
             switch (color) {
-
                 case "a":
                     if (a) {
                         System.out.println("a is already taken");
